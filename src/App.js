4289aee48c2611aa59e953cccac6e3a9ee725f1d
ekalii <andoune0719@gmail.com>
2020-06-24 14:16:59 +0900

    #1 making frame using just html

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <header className="main_header">Project Board</header>
+      <section className="main_section">
+  			<article className="todo">Hello</article>
+  			<article className="in_progress">Hello</article>
+  			<article className="done">Hello</article>
+      </section>
+      <footer className="main_footer">개발자:<a href="https://github.com/Ekalii">&nbsp;Ekalii</a></footer>
     </div>
   );
 }
