dbd930744381b70256cd1818c38e3d97a44a3de2
ekalii <andoune0719@gmail.com>
2020-06-28 09:45:27 +0900

    code refactoring: delete test case

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
 import React from 'react';
 import './App.css';
-import { createStore, combineReducers } from 'redux';
-import postReducer, {
-  addPost,
-  removePost,
-  editPost
-} from './post/state';
-import friendReducer, {
-  addFriend,
-  removeFriend
-} from './friend/state';
 import FriendMain from './friend/container/FriendMain';
 import PostMain from './post/container/PostMain';
-
-const reducer = combineReducers({
-  post: postReducer,
-  friend: friendReducer
-})
-
-const store = createStore(reducer);
-store.subscribe(() => {
-  const state = store.getState();
-  console.log(state);
-})
-
-store.dispatch(addPost({ id: 1, desc: 'love coding'}));
-store.dispatch(addPost({ id: 2, desc: 'hello world!'}));
-store.dispatch(editPost({ id: 1, desc: 'like coding'}));
-
-store.dispatch(addFriend({ id: 1, name: 'IU'}));
 
 function App() {
   return (
